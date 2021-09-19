5aeece388cd7fcfb9f2ea91b2edac6c6d2b19625
Shiv <shivsaggu@hotmail.com>
2021-09-18 22:27:09 -0400

    modified code

--- server.js
+++ server.js
@@ -17,4 +17,32 @@ app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/em999v2/'}),
 );
 
+
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
+app.configure(function () {
+    app.use(allowCrossDomain);
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(application_root, "public")));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  });
+
+
+
 app.listen(process.env.PORT || 8080);
