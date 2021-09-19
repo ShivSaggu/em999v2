e880ae56fef3912bfa47808e619fa745455c633f
Shiv <shivsaggu@hotmail.com>
2021-09-18 22:58:42 -0400

    modified code

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ var cors = require('cors');
 var app = express();
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 
 app.use(requireHTTPS);
 app.use(express.static('./dist/em999v2'));
@@ -21,13 +29,7 @@ app.get('/*', (req, res) =>
 );
 
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+ 
 
 
 app.listen(process.env.PORT || 8080);
