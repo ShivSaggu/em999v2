960b4f3de239cef414d6978808bf0638b872322c
Shiv <shivsaggu@hotmail.com>
2021-09-18 22:05:08 -0400

    modified code

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -8,10 +8,12 @@ export class DataService {
 
   url="https://em999-api.herokuapp.com/api/events";
 
+  url2="https://jsonplaceholder.typicode.com/todos";
+
   constructor(private http:HttpClient) { }
 
   getdata(){
-    return this.http.get(this.url);
+    return this.http.get(this.url2);
   }
 
 
