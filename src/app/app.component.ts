0ba31725337bd9e1110f574b8592c723747b6c33
Shiv <ssaggu@deloitte.com>
2021-09-19 20:42:30 -0400

    modify code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'em999v2';
+
+  const query = {
+    // give the query a unique name
+    name: 'fetch-user',
+    text: 'select first_name__c from salesforce.event_attendee__c',
+    values: [1],
+  }
+  // callback
+  client.query(query, (err, res) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      console.log(res.rows[0])
+    }
+  })
+
+
 }
